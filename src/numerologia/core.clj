c6cf092e0d9d375449e3a172f02c7631fca732b9
Guto Marcicano <caugmar@gmail.com>
2011-01-03 16:58:43 -0200

    Primeira parte.

--- src/numerologia/core.clj
+++ src/numerologia/core.clj
@@ -3,3 +3,23 @@
 (defn -main []
   (println "Olá, mundo!"))
 
+(defn criar-tabela [origem destino] 
+  (let [letras (map (fn [c] c) origem)
+        valores (map (fn [c] (Integer. (str c))) destino)]
+    (apply hash-map (interleave letras valores))))
+
+(def alfabeto-completo (criar-tabela "abcdefghijklmnopqrstuvwxyz"
+                                     "12345678912345678912345678"))
+
+(def consoantes (criar-tabela "abcdefghijklmnopqrstuvwxyz"
+                              "02340678012345078912045678"))
+
+(def vogais (criar-tabela "abcdefghijklmnopqrstuvwxyz"
+                          "10005000900000600000300000"))
+
+(def sem-acento (apply hash-map (map (fn [c] c) "áaéeíióoúuàaèeìiòoùuâaêeîiôoûuãaẽeĩiõoũuäaëeïiöoüuçcñn")))
+
+(def pontuação #{\space \' \! \@ \# \$ \% \¨ \& \* \( \) \_ \+ \- \= \` \´ \{ \[ \^ \~ \} \] \ª \º \< \> \, \. \: \; \? \/ \| \" \\ })
+
+(defn converter-nome [nome]
+  (map (fn [c] c) (.toLowerCase nome)))
