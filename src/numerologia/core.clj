f3734fd5c0b4d12754f52a0fa807a18bc0b5bcbf
Guto Marcicano <caugmar@gmail.com>
2011-01-06 14:28:10 -0200

    Cálculo completo.

--- src/numerologia/core.clj
+++ src/numerologia/core.clj
@@ -1,11 +1,9 @@
 (ns numerologia.core)
 
-(defn -main [] (println "Olá, mundo!"))
-
 (defn separar-digitos [numero] (map (fn [c] (Integer. (str c))) (str numero)))
 
 (defn criar-tabela [origem destino] 
-  (let [letras (map (fn [c] c) origem)
+  (let [letras (seq origem)
         valores (separar-digitos destino)]
     (apply array-map (interleave letras valores))))
 
@@ -18,7 +16,8 @@
 (def vogais (criar-tabela "abcdefghijklmnopqrstuvwxyz"
                           "10005000900000600000300000"))
 
-(def sem-acento (apply array-map (map (fn [c] c) "áaéeíióoúuàaèeìiòoùuâaêeîiôoûuãaẽeĩiõoũuäaëeïiöoüuçcñn")))
+(def sem-acento (apply array-map (interleave (seq "áéíóúàèìòùâêîôûãẽĩõũäëïöüçñ")
+                                             (seq "aeiouaeiouaeiouaeiouaeioucn")
 
 (def pontuação #{\space \' \! \@ \# \$ \% \¨ \& \* \( \) \_ \+ \- \= \` \´ \{ \[ \^ \~ \} \] \ª \º \< \> \, \. \: \; \? \/ \| \" \\ })
 
@@ -33,5 +32,15 @@
       resultado
       (reduzir (separar-digitos resultado)))))
 
-(println (reduzir (map alfabeto-completo (converter-nome "Carlos Augusto Marcicano"))))
+(defn converter-data [data] (map #(Integer. (str %)) (remove (fn [c] (= c \/)) data)))
+
+(defn obter-numero-do-caminho-da-vida [data] (reduzir (converter-data data)))
+
+(defn obter-numero-do-destino [nome] (reduzir (map alfabeto-completo (converter-nome nome))))
+
+(defn obter-numero-do-desejo-da-alma [nome] (reduzir (map vogais (converter-nome nome))))
+
+(defn obter-numero-dos-sonhos-interiores [nome] (reduzir (map consoantes (converter-nome nome))))
+
+(defn obter-numero-do-aniversario [data] (Integer. (apply str (take 2 data))))
 
