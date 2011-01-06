dff67acb93883a97a014db59c3447fef89873c4a
Guto Marcicano <caugmar@gmail.com>
2011-01-06 13:55:30 -0200

    Mais adiantado.

--- src/numerologia/core.clj
+++ src/numerologia/core.clj
@@ -1,12 +1,13 @@
 (ns numerologia.core)
 
-(defn -main []
-  (println "Olá, mundo!"))
+(defn -main [] (println "Olá, mundo!"))
+
+(defn separar-digitos [numero] (map (fn [c] (Integer. (str c))) (str numero)))
 
 (defn criar-tabela [origem destino] 
   (let [letras (map (fn [c] c) origem)
-        valores (map (fn [c] (Integer. (str c))) destino)]
-    (apply hash-map (interleave letras valores))))
+        valores (separar-digitos destino)]
+    (apply array-map (interleave letras valores))))
 
 (def alfabeto-completo (criar-tabela "abcdefghijklmnopqrstuvwxyz"
                                      "12345678912345678912345678"))
@@ -17,9 +18,20 @@
 (def vogais (criar-tabela "abcdefghijklmnopqrstuvwxyz"
                           "10005000900000600000300000"))
 
-(def sem-acento (apply hash-map (map (fn [c] c) "áaéeíióoúuàaèeìiòoùuâaêeîiôoûuãaẽeĩiõoũuäaëeïiöoüuçcñn")))
+(def sem-acento (apply array-map (map (fn [c] c) "áaéeíióoúuàaèeìiòoùuâaêeîiôoûuãaẽeĩiõoũuäaëeïiöoüuçcñn")))
 
 (def pontuação #{\space \' \! \@ \# \$ \% \¨ \& \* \( \) \_ \+ \- \= \` \´ \{ \[ \^ \~ \} \] \ª \º \< \> \, \. \: \; \? \/ \| \" \\ })
 
-(defn converter-nome [nome]
-  (map (fn [c] c) (.toLowerCase nome)))
+(defn converter-nome [nome] (remove pontuação (map (fn [c] (get sem-acento c c)) (.toLowerCase nome))))
+
+(defn reduzir [numeros]
+  (let [resultado (apply + numeros)]
+    (if (or
+          (and (>= resultado 1) (<= resultado 9))
+          (= resultado 11)
+          (= resultado 22))
+      resultado
+      (reduzir (separar-digitos resultado)))))
+
+(println (reduzir (map alfabeto-completo (converter-nome "Carlos Augusto Marcicano"))))
+
